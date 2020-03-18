e86c2191b7ad1709a406882bfaad4588a1ccbb69
Arohan Dutt <arohandutt@gmail.com>
2020-03-17 19:43:26 -0500

    Add route for modifying count

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ TODO:
 Add row to table
 Get count via hourLogID
 Get counts via user
-Reduce count 
+Modify count 
 Change date
 Delete row
 
@@ -27,7 +27,7 @@ app.use(express.static('dist'));
 
 // ADD ROW TO TABLE
 app.post('/addLog', (req, res) => {
-  
+
   const { user, log_name, remaining } = req.query
 
   knex('hour_logs').insert({
@@ -35,23 +35,23 @@ app.post('/addLog', (req, res) => {
     log_name: log_name,
     remaining: remaining
   })
-  .then(() => {
-    res.end(`Row added to table for log ${log_name}`)
-  })
-  .catch(res.end)
+    .then(() => {
+      res.end(`Row added to table for log ${log_name}`)
+    })
+    .catch(res.end)
 
 })
 
 // Get count of log
 app.get('/logHours', (req, res) => {
-  
+
   knex.select('remaining').from('hour_logs').where({
     log_id: +req.query.id
   })
-  .then((result) => {
-    res.send(result);
-  })
-  .catch(res.end)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch(res.end)
 })
 
 // Get user logs
@@ -59,10 +59,10 @@ app.get('/userLogs', (req, res) => {
   knex.select().from('hour_logs').where({
     user: req.query.user
   })
-  .then((result) => {
-    res.send(result);
-  })
-  .catch(res.end)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch(res.end)
 })
 
 // Verify
@@ -75,13 +75,33 @@ const verifyUser = async (req, log_id) => {
 }
 
 // Tests
-// let dummyReq = {query: {user: 'postman'}}
+// let dummyReq = {query: {user: 'mark'}}
 // verifyUser(dummyReq, 2).then(console.log)
+
+// Modify Count
+app.patch('/modifyCount', (req, res) => {
+  verifyUser(req, req.query.log_id)
+    .then((veri) => {
+      if (!veri) {
+        res.status(401).send('Unable to authorize request')
+      } else {
+        knex('hour_logs')
+          .where({ log_id: +req.query.log_id })
+          .update({ remaining: +req.query.new_value })
+          .then(() => {
+            res.end(`Record ${req.query.log_id} was updated`)
+          })
+          .catch((err) => res.end(err.toString()))
+      }
+    })
+})
+
+
 
 
 // ROUTER (add new routes above)
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../dist/index.html'), function(err) {
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'), function (err) {
     if (err) {
       res.status(500).send(err)
     }
