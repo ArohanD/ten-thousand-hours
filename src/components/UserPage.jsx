5369a40e9da642fed20d473bed87b7976e8d4fa4
Arohan Dutt <arohandutt@gmail.com>
2020-04-25 18:52:38 -0400

    Display userLogs

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,24 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const UserPage = (props) => {
   const [userData, setUserData] = useState([])
   
-  axios.get('/userLogs', {
-    params: {
-      user: props.user
-    }
-  })
-  .then(({data}) => {
-    setUserData(data)
-  })
+  useEffect(() => {
+    axios.get('/userLogs', {
+      params: {
+        user: props.user
+      }
+    })
+    .then(({data}) => {
+      console.log(data)
+      setUserData(data)
+    })
+  }, [])
 
   return (
     <div>
-      {userData.toString()}
+      <div id="log_container">
+        {userData.map((log, index) => <Log log={log} key={index} />)}
+      </div>
     </div>
   )
 }
+
+const Log = (props) => {
+  return (
+    <div>
+      {`${props.log.log_name} + ${props.log.remaining} + ${props.log.created_at}`}
+    </div>
+  )
+} 
 
 export default (props) => {
   return <UserPage user={'postman'}/>
